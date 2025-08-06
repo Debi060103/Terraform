d7dfa76914e2044d8676efe6be4061bd175adf46
Debi060103 <debidattachhotaray123@gmail.com>
2025-08-06 12:29:40 +0530

    added terraform.tfvars

--- Day-2-main-config-file/variables.tf
+++ Day-2-main-config-file/variables.tf
@@ -1,12 +1,12 @@
 variable "ami-id" {
   description = "inserting value in this"
   type = string
-  default ="ami-08a6efd148b1f7504"
+  default =""
 }
 
 variable "type" {
     description = "instance type inserting in main.tf"
     type = string
-    default = "t3.micro"
+    default = ""
   
 }
